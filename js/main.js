0b1b14b71108e3822813529171a0ea2b20ad5fea
Gheorghe Rapesco <gheorgherapesco@gmail.com>
2025-02-06 12:57:22 +0200

    Specializari update 1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function handleScroll() {
-    const header = document.querySelector('.transparent-header');
+    const header = document.querySelector('.main-header');
     if (header.classList.contains('open')) {
         
         window.scrollTo({
@@ -18,6 +18,31 @@ hamMenu.addEventListener('click', () => {
 
     handleScroll();
 });
+
+
+
+const accordion = document.querySelector(".accordion");
+accordion.addEventListener("click", (e) => {
+    const activePanel = e.target.closest(".accordion-panel");
+    if(!activePanel) return;
+    toggleAccordion(activePanel);
+})
+
+function toggleAccordion(panelToActivate) {
+    const buttons = panelToActivate.parentElement.querySelectorAll('button');
+    const contents = panelToActivate.parentElement.querySelectorAll('.accordion-content');
+
+    buttons.forEach((button) => {
+        button.setAttribute('aria-expanded', false)
+    });
+
+    contents.forEach((contents) => {
+        contents.setAttribute('aria-hidden', true)
+    });
+
+    panelToActivate.querySelector('button').setAttribute('aria-expanded', true);
+    panelToActivate.querySelector('.accordion-content').setAttribute('aria-hidden', false);
+}
 
 
 document.addEventListener("DOMContentLoaded", function() {
